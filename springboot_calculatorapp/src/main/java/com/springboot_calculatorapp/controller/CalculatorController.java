5630d096e7c6a1b9c2e6c6abdbd788f2e1311055
omkar3720 <omkarshinde2728@gmail.com>
2024-09-28 12:27:58 +0530

    Update CalculatorController.javabyakshay

--- springboot_calculatorapp/src/main/java/com/springboot_calculatorapp/controller/CalculatorController.java
+++ springboot_calculatorapp/src/main/java/com/springboot_calculatorapp/controller/CalculatorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class CalculatorController 
 {
-@RequestMapping("/calcu")	
+@RequestMapping("/cal")	
 public String calculator()
 {
 	return "calculator";
